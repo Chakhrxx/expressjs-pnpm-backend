d60db397c2625fe506cc63775eefa2ec2c42b2a9
Chakhrxx <chakhree.h@gmail.com>
2023-03-17 06:42:07 +0700

    https://expressjs.com/

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.redirect("https://expressjs.com/");
 });
 
 app.listen(3000, () => {
